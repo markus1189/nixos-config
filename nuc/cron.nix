c9af4940c001bbf31423bfca249cac31b92ecb1d
Markus Hauck <markus1189@gmail.com>
2019-01-06 19:53:45 +0100

    New cron schedules for downloads

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -2,10 +2,13 @@ userName:
 { config, pkgs, ...}:
 
 let
+  downloadDir = "$HOME/Downloads";
   killall = "${pkgs.psmisc}/bin/killall";
   kodi = "${pkgs.kodi}/bin/kodi";
   pgrep = "${pkgs.procps}/bin/pgrep";
+  find = "${pkgs.findutils}/bin/find";
   secrets = import ../nixos-shared/secrets.nix;
+  rm = "${pkgs.coreutils}/bin/rm";
   in-or-out = import ./in-or-out.nix {
     inherit (userPkgs) writeShellScriptBin;
     inherit (pkgs) curl;
@@ -19,12 +22,15 @@ in
     cron = {
       enable = true;
       systemCronJobs = [
-        "30 15 * * thu ${userName} ${in-or-out}/bin/in-or-out"
-        " 0  3 * * * ${userName} ${killall} kodi 2>/dev/null"
-        " 0  3 * * * ${userName} ${killall} kodi.bin 2>/dev/null"
-        " 5  3 * * * ${userName} ${killall} -9 kodi 2>/dev/null"
-        " 5  3 * * * ${userName} ${killall} -9 kodi.bin 2>/dev/null"
-        "10 20 * * * ${userName} ${pgrep} kodi || DISPLAY=:0 ${kodi} &>/dev/null &"
+        "30          15 * * thu ${userName} ${in-or-out}/bin/in-or-out"
+        " 0           3 * *   * ${userName} ${killall} kodi 2>/dev/null"
+        " 0           3 * *   * ${userName} ${killall} kodi.bin 2>/dev/null"
+        " 5           3 * *   * ${userName} ${killall} -9 kodi 2>/dev/null"
+        " 5           3 * *   * ${userName} ${killall} -9 kodi.bin 2>/dev/null"
+        "10          20 * *   * ${userName} ${pgrep} kodi || DISPLAY=:0 ${kodi} &>/dev/null &"
+        " 0           * * *   * ${userName} ${find} ${downloadDir} -type f \\( -iname '*.idx' -or -iname '*.nfo' -or -iname '*.sfv' -or -iname '*.url' -or -iname '*.rev' -or -iname '*[.-]sample.[ma][kv][vi]' \\) -delete"
+        " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
+        "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
       ];
     };
   };
