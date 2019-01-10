5e7ad39aba49bdea7fccf3e37f9a1489725f8560
Markus Hauck <markus1189@gmail.com>
2019-01-10 19:44:34 +0100

    Remove cron job to delete files

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -21,7 +21,6 @@ in
         " 5           3 * *   * ${userName} ${killall} -9 kodi 2>/dev/null"
         " 5           3 * *   * ${userName} ${killall} -9 kodi.bin 2>/dev/null"
         "10          20 * *   * ${userName} ${pgrep} kodi || DISPLAY=:0 ${kodi} &>/dev/null &"
-        " 0           * * *   * ${userName} ${find} ${downloadDir} -type f \\( -iname '*.idx' -or -iname '*.nfo' -or -iname '*.sfv' -or -iname '*.url' -or -iname '*.rev' -or -iname '*[.-]sample.[ma][kv][vi]' \\) -delete"
         " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
         "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
       ];
