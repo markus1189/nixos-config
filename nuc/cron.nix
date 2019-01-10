0c0aeaae8f397e7b2bfc1fc95018799df57a2e6e
Markus Hauck <markus1189@gmail.com>
2019-01-10 19:43:09 +0100

    Migrate in-or-out to gitlab

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -9,12 +9,6 @@ let
   find = "${pkgs.findutils}/bin/find";
   secrets = import ../nixos-shared/secrets.nix;
   rm = "${pkgs.coreutils}/bin/rm";
-  in-or-out = import ./in-or-out.nix {
-    inherit (userPkgs) writeShellScriptBin;
-    inherit (pkgs) curl;
-    channel = "#ffm";
-    token = secrets.ffmBotToken;
-  };
   userPkgs = import ../nixos-shared/scripts/scripts.nix { inherit pkgs; };
 in
 {
@@ -22,7 +16,6 @@ in
     cron = {
       enable = true;
       systemCronJobs = [
-        "30          15 * * thu ${userName} ${in-or-out}/bin/in-or-out"
         " 0           3 * *   * ${userName} ${killall} kodi 2>/dev/null"
         " 0           3 * *   * ${userName} ${killall} kodi.bin 2>/dev/null"
         " 5           3 * *   * ${userName} ${killall} -9 kodi 2>/dev/null"
