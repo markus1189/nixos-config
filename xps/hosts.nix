06b1d1aed862fb24862540bf7d7bf2cdccf1a180
Markus Hauck <markus1189@gmail.com>
2019-07-16 21:30:03 +0200

    Fix hosts file

--- xps/hosts.nix
+++ xps/hosts.nix
@@ -1,17 +1,15 @@
 { config, pkgs, ... }:
 
 let
-  hostsCommit = "b7d496e9649df080f3bdb88b8c4afa3faf2c5237";
+  hostsCommit = "5386cec268756eaeb2348c1bdffbe961a8097898";
   hostsFile = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/StevenBlack/hosts/${hostsCommit}/alternates/fakenews-gambling-porn/hosts";
-    sha256 = "1ni9hj9g0v3hssbfcgwavvb4za0rvvzp5cdjj10x2mxqcd6lnv6n";
+    sha256 = "05kiwmn4nn45v8k3d3hi2nn2rjhdx057plx5r5yarwiqw2m4x7x1";
   };
   modifiedHosts = pkgs.runCommand "filtered-hosts" {} ''
     echo "# Modifed version of ${hostsFile}" > $out
     # Accept this because of farnam street links...
-    ${pkgs.gnused}/bin/sed -i '/link.m.convertkit/d' $out
-    # For breuninger development
-    ${pkgs.gnused}/bin/sed -i '/richrelevance.com/d' $out
+    ${pkgs.gnused}/bin/sed '/link.m.convertkit/d' ${hostsFile} > $out
   '';
 in
 {
