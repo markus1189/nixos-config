d08a9371aaa946fd26a096b5b7dd33f063c8780c
Markus Hauck <markus1189@gmail.com>
2019-12-22 19:59:17 +0100

    Allow scrolller website

--- xps/hosts.nix
+++ xps/hosts.nix
@@ -2,13 +2,12 @@
 
 let
   pinnedHostsVersion = pkgs.lib.importJSON ../pinned-versions/hosts-version.json;
-  hostsCommit = "5386cec268756eaeb2348c1bdffbe961a8097898";
   hostsRepo = pkgs.fetchgit {
     inherit (pinnedHostsVersion) url rev sha256 fetchSubmodules;
   };
   modifiedHosts = pkgs.runCommand "filtered-hosts" {} ''
     # Accept this because of farnam street links...
-    ${pkgs.gnused}/bin/sed '/link.m.convertkit/d' ${hostsRepo}/alternates/fakenews-gambling-porn/hosts > $out
+    ${pkgs.gnused}/bin/sed -e '/link.m.convertkit/d' -e '/scrolller.com/d' ${hostsRepo}/alternates/fakenews-gambling-porn/hosts > $out
   '';
 in
 {
