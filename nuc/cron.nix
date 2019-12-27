bc22b3957a6f759c8026eab0a3ee8b2a1ceb229a
Markus Hauck <markus1189@gmail.com>
2019-12-27 09:56:07 +0100

    Remove kodi cron jobs from nuc

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -15,11 +15,6 @@ in
     cron = {
       enable = true;
       systemCronJobs = [
-        " 0           3 * *   * ${userName} ${killall} kodi 2>/dev/null"
-        " 0           3 * *   * ${userName} ${killall} kodi.bin 2>/dev/null"
-        " 5           3 * *   * ${userName} ${killall} -9 kodi 2>/dev/null"
-        " 5           3 * *   * ${userName} ${killall} -9 kodi.bin 2>/dev/null"
-        "10          20 * *   * ${userName} ${pgrep} kodi || DISPLAY=:0 ${kodi} &>/dev/null &"
         " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
         "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
       ];
