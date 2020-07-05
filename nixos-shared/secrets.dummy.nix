01ad2032fc138efd6cc094bd0b5b2a4226fe7c7f
Markus Hauck <markus1189@gmail.com>
2020-07-05 18:48:39 +0200

    Add dummy secrets file

---
+++ nixos-shared/secrets.dummy.nix
@@ -0,0 +1,21 @@
+let dummy = "just-a-dummy-value"; in
+  {
+  x11vnc = {
+    password = dummy;
+  };
+
+  googlepw = dummy;
+
+  restic = {
+    b2bucket = {
+      name = dummy;
+      password = dummy;
+      account-id = dummy;
+      account-key = dummy;
+    };
+  };
+
+  pushBulletToken = dummy;
+
+  telegramBotToken = dummy;
+}
