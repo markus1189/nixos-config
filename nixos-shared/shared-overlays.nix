c20451737712324cc8ce54d83f1c516bf26fbf77
Markus Hauck <markus1189@gmail.com>
2020-07-27 11:25:09 +0200

    Add overlay to fix ddgr

--- nixos-shared/shared-overlays.nix
+++ nixos-shared/shared-overlays.nix
@@ -1,23 +1,38 @@
 rec {
   stableNixpkgsOverlay = self: super: {
-    stableNixpkgs = import self.ndtSources.nixpkgs-2003 {};
+    stableNixpkgs = import self.ndtSources.nixpkgs-2003 { };
   };
+
   nixpkgs1703Overlay = self: super: {
-    nixpkgs1703 = import self.ndtSources.nixpkgs-1703 {};
+    nixpkgs1703 = import self.ndtSources.nixpkgs-1703 { };
   };
+
   bukuOverlay = self: super: {
-    buku = builtins.trace "INFO: using pinned buku version" self.stableNixpkgs.buku;
+    buku =
+      builtins.trace "INFO: using pinned buku version" self.stableNixpkgs.buku;
   };
+
   ndtOverlay = self: super: {
-    ndt = import (builtins.fetchTarball https://github.com/markus1189/ndt/archive/master.tar.gz) {
-      nixpkgs = self;
-    };
+    ndt = import (builtins.fetchTarball
+      "https://github.com/markus1189/ndt/archive/master.tar.gz") {
+        nixpkgs = self;
+      };
   };
+
   ndtSourcesOverlay = self: super: {
-    ndtSources = import ../ndt/sources.nix {};
+    ndtSources = import ../ndt/sources.nix { };
   };
+
   dunstWithDunstify = self: super: {
     dunst = super.dunst.override { dunstify = true; };
+  };
+
+  temporaryDdgrFix = self: super: {
+    ddgr = builtins.trace
+      "INFO: using overlay for ddgr until https://github.com/NixOS/nixpkgs/pull/93928"
+      (self.callPackage (builtins.fetchurl
+        "https://raw.githubusercontent.com/markus1189/nixpkgs/ddgr-1-9/pkgs/applications/misc/ddgr/default.nix")
+        { });
   };
 
   overlays = [
@@ -27,5 +42,6 @@ rec {
     stableNixpkgsOverlay
     nixpkgs1703Overlay
     dunstWithDunstify
+    temporaryDdgrFix
   ];
 }
