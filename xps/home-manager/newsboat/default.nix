5f20e7a63c0a30586388fb33346addab3c319624
Markus Hauck <markus1189@gmail.com>
2020-09-26 20:17:38 +0200

    Add hgon scraper to newsboat

--- xps/home-manager/newsboat/default.nix
+++ xps/home-manager/newsboat/default.nix
@@ -1,16 +1,67 @@
-{ lib, writeScriptBin, secrets }:
+{ lib, writeScript, writeScriptBin, secrets, curl, jq, pup }:
 
 let
+  jsonToRssScript = writeScript "json-to-rss" ''
+    # Expects the following format:
+
+    # [
+    #   {
+    #     "pubDate": "",
+    #     "title": "",
+    #     "link": "",
+    #     "description": ""
+    #   }
+    # ]
+
+    TITLE="''${1:?no-title-given}"
+    DESCRIPTION="''${2:?no-description-given}"
+    LINK="''${3:?no-link-given}"
+
+    cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+<title>$TITLE</title>
+<description>$DESCRIPTION</description>
+<link>$LINK</link>
+
+$(
+  ${jq}/bin/jq -r 'map([
+    "<item>",
+    "<title>\(.title)</title>",
+    "<link>\(.link)</link>",
+    "<guid>\(.link)</guid>",
+    "<pubDate>\(.pubDate)</pubDate>",
+    "</item>"
+  ] | join("\n")) | join("\n")'
+)
+
+</channel>
+</rss>
+EOF
+  '';
+  scrapeHgonScript = writeScript "scrape-hgon.sh" ''
+    getItems() {
+        ${curl}/bin/curl -s --fail https://www.hgon.de/entdecken/ | ${pup}/bin/pup '.main__content .main__content article json{}'
+    }
+
+    buildItems() {
+        ${jq}/bin/jq 'map({pubDate: .. | select(try .tag == "time") | .datetime | strptime("%Y.%m.%d ") | strftime("%a, %d %b %Y %H:%M:%S GMT"), title: .. | select(try .tag == "h3") | .text, link: "https://www.hgon.de/\(.. | select(try .tag == "a") | .href)", description: .. | select(try .class == "card__content") | .children | map(.text) | join(" ")})'
+    }
+
+    getItems | buildItems | ${jsonToRssScript} "HGON Entdecken (Manual Scrape)" "Manual scrape of HGON news" "https://www.hgon.de/entdecken"
+  '';
   urlsFile = ./urls;
   urlLines = lib.splitString "\n" (builtins.readFile urlsFile);
-  addToPocketScript = writeScriptBin "add-to-pocket.sh" ''
+  urlExecs = [
+    "exec:${scrapeHgonScript}"
+  ];
+  addToPocketScript = writeScript "add-to-pocket.sh" ''
     script_pocket_consumer_key=${secrets.pocket.consumer_key}
     script_pocket_access_token=${secrets.pocket.access_token}
 
     main() {
-        echo "Body is $(jo url="''${1}" consumer_key="''${script_pocket_consumer_key}" access_token="''${script_pocket_access_token}")" >> /tmp/log
-
-        curl -XPOST https://getpocket.com/v3/add -H 'content-type: application/json' -d "$(jo url="''${1}" consumer_key="''${script_pocket_consumer_key}" access_token="''${script_pocket_access_token}")"
+        ${curl}/bin/curl -s -XPOST https://getpocket.com/v3/add -H 'content-type: application/json' -d "$(jo url="''${1}" consumer_key="''${script_pocket_consumer_key}" access_token="''${script_pocket_access_token}")"
     }
 
     main "$1"
@@ -20,28 +71,32 @@ in {
     enable = true;
     autoReload = true;
     reloadTime = 10;
-    urls = map (url: { inherit url; }) urlLines;
+    urls = map (url: { inherit url; }) (urlLines ++ urlExecs);
     extraConfig = ''
+      cleanup-on-quit no
+      delete-read-articles-on-quit no
       show-read-feeds no
       confirm-exit yes
+      download-full-page yes
+      history-limit 9999
 
-      color background white black bold
-      color listnormal white black
-      color listnormal_unread white black bold
-      color listfocus black color214
-      color listfocus_unread black color214 bold
-      color info color214 black bold
-      color article white black
+      color background white default bold
+      color listnormal white default
+      color listnormal_unread white default bold
+      color listfocus color0 color172
+      color listfocus_unread color0 color172 bold
+      color info color172 black bold
+      color article white default
 
       bind-key j down
       bind-key k up
       bind-key g home
       bind-key G end
 
-      macro , bookmark ; toggle-article-read
-      macro A mark-feed-read ; quit
+      macro . bookmark ; toggle-article-read
+      macro , mark-feed-read ; next-unread-feed
 
-      bookmark-cmd "${addToPocketScript}/bin/add-to-pocket.sh"
+      bookmark-cmd "${addToPocketScript}"
       bookmark-autopilot yes
 
       define-filter "this week" "age <= 7"
