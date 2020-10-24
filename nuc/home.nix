3b9df5def25af514d52a294cc51df21161d089c0
Markus Hauck <markus1189@gmail.com>
2020-10-24 19:41:23 +0200

    nuc: install newsboat and rsstail services

--- nuc/home.nix
+++ nuc/home.nix
@@ -5,12 +5,6 @@ let
   mergeAttrList = pkgs.lib.foldl' pkgs.lib.mergeAttrs { };
 in {
   home = {
-    # packages = [
-    #   pkgs.source-code-pro
-    #   pkgs.dunst
-    #   pkgs.ndt
-    #   pkgs.haskellPackages.brittany
-    # ];
   };
 
   manual = {
@@ -19,55 +13,38 @@ in {
     manpages.enable = true;
   };
 
-  # programs = {
-  #   bash.enable = true;
-  #   zsh.enable = true;
-  #   direnv = {
-  #     enable = true;
-  #     enableBashIntegration = true;
-  #     enableNixDirenvIntegration = true;
-  #     enableZshIntegration = true;
-  #   };
-
-  #   firefox.enable = true;
-
-  #   git =
-  #     (pkgs.callPackage ../nixos-shared/home-manager/git/default.nix { }).value;
-
-  #   newsboat =
-  #     (pkgs.callPackage ../nixos-shared/home-manager/newsboat/default.nix {
-  #       inherit secrets;
-  #     }).value;
-
-  #   vim =
-  #     (pkgs.callPackage ../nixos-shared/home-manager/vim/default.nix { }).value;
-  # };
+  programs = {
+    newsboat =
+      (pkgs.callPackage ../nixos-shared/home-manager/newsboat/default.nix {
+        inherit secrets;
+      }).value;
+  };
 
   services = {
   };
 
   fonts = { fontconfig = { enable = true; }; };
 
-  # systemd.user = {
-  #   startServices = true;
-  #   services = let rsstail = pkgs.mkRsstailToPocketUnitWithSecrets;
-  #   in mergeAttrList (map rsstail [
-  #     {
-  #       key = "xkcd";
-  #       url = "http://www.xkcd.com/rss.xml";
-  #     }
-  #     {
-  #       key = "commitstrip";
-  #       url = "http://www.commitstrip.com/en/feed/";
-  #     }
-  #     {
-  #       key = "raptitude";
-  #       url = "https://www.raptitude.com/feed/";
-  #     }
-  #     {
-  #       key = "farnamstreet";
-  #       url = "fs.blog/feed";
-  #     }
-  #   ]);
-  # };
+  systemd.user = {
+    startServices = true;
+    services = let rsstail = pkgs.mkRsstailToPocketUnitWithSecrets;
+    in mergeAttrList (map rsstail [
+      {
+        key = "xkcd";
+        url = "http://www.xkcd.com/rss.xml";
+      }
+      {
+        key = "commitstrip";
+        url = "http://www.commitstrip.com/en/feed/";
+      }
+      {
+        key = "raptitude";
+        url = "https://www.raptitude.com/feed/";
+      }
+      {
+        key = "farnamstreet";
+        url = "fs.blog/feed";
+      }
+    ]);
+  };
 }
