1011a5bc0cfa0ea9c4126b53fb96c04e02a74ade
Markus Hauck <markus1189@gmail.com>
2020-12-14 14:56:04 +0100

    Set some vim defaults

--- nixos-shared/home-manager/vim/default.nix
+++ nixos-shared/home-manager/vim/default.nix
@@ -5,5 +5,13 @@
 {
   value = {
     enable = true;
+    extraConfig = ''
+      " https://swordandsignals.com/2020/12/13/5-lines-in-vimrc.html
+      set hlsearch    " highlight all search results
+      set ignorecase  " do case insensitive search
+      set incsearch   " show incremental search results as you type
+      set number      " display line number
+      set noswapfile  " disable swap file
+    '';
   };
 }
