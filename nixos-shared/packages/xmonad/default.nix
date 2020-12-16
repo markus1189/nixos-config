defa88485bcb01535cd6705ac2980a0b703abb04
Markus Hauck <markus1189@gmail.com>
2020-12-16 18:33:39 +0100

    xmonad: Use autorandr

--- nixos-shared/packages/xmonad/default.nix
+++ nixos-shared/packages/xmonad/default.nix
@@ -2,7 +2,7 @@ args@{ mutate, xmobar, xkill, xmobarLower, xmobarUpper, playerctl
 , selectSpotifyPlayer, rofi, chooseNetwork, xmonadReset, singlehead
 , takeScreenshot, lockScreen, centerMouse, multihead4k, emacsAnywhere, mfaHelper
 , clipmenu, rofiDefaults, autoMonitorConfig, tmx, browserHistory, bukuRun, ddgr
-, flameshot
+, flameshot, autorandr
 }:
 
 mutate ./xmonad.hs (builtins.removeAttrs args [ "mutate" ])
