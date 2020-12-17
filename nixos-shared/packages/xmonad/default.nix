ec8e96722cd54239cfdc8ebb4f44222166e5db0f
Markus Hauck <markus1189@gmail.com>
2020-12-17 08:16:31 +0100

    xmonad: cleanup xmobar

--- nixos-shared/packages/xmonad/default.nix
+++ nixos-shared/packages/xmonad/default.nix
@@ -1,8 +1,7 @@
 args@{ mutate, xmobar, xkill, xmobarLower, xmobarUpper, playerctl
 , selectSpotifyPlayer, rofi, chooseNetwork, xmonadReset, singlehead
-, takeScreenshot, lockScreen, centerMouse, multihead4k, emacsAnywhere, mfaHelper
-, clipmenu, rofiDefaults, autoMonitorConfig, tmx, browserHistory, bukuRun, ddgr
-, flameshot, autorandr
-}:
+, takeScreenshot, lockScreen, centerMouse, multihead4k, emacsAnywhere, clipmenu
+, rofiDefaults, autoMonitorConfig, tmx, browserHistory, bukuRun, ddgr, flameshot
+, autorandr }:
 
 mutate ./xmonad.hs (builtins.removeAttrs args [ "mutate" ])
