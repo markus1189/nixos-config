65eee173109cc1f3fd5a464c4847b13decad59c8
Markus Hauck <markus1189@gmail.com>
2021-01-06 19:20:11 +0100

    Also build p1's configuration

--- default.nix
+++ default.nix
@@ -2,4 +2,4 @@ let
   builder = configuration:
     (import <nixpkgs/nixos> { inherit configuration; }).system;
 
-in map builder [ ./xps/configuration.nix ./nuc/configuration.nix ]
+in map builder [ ./xps/configuration.nix ./nuc/configuration.nix ./p1/configuration.nix ]
