f9f1109d212203cb3d7461ee1486b750d1e11c7b
Markus Hauck <markus1189@gmail.com>
2021-02-01 20:18:11 +0100

    xmonad: haskell-language-server setup in addition to shell

--- nixos-shared/packages/xmonad/shell.nix
+++ nixos-shared/packages/xmonad/shell.nix
@@ -2,6 +2,6 @@ with import <nixpkgs> { };
 
 mkShell {
   buildInputs = [
-    (haskellPackages.ghcWithPackages (ps: with ps; [ xmonad xmonad-contrib ]))
+    (haskellPackages.ghcWithPackages (ps: with ps; [ xmonad xmonad-contrib haskell-language-server ]))
   ];
 }
