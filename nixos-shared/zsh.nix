cf69782e969e97241a577edddbfa1c2cde39ca9b
Markus Hauck <markus1189@gmail.com>
2021-02-06 18:47:37 +0100

    zsh,laptop: move zbell out from common config
    zbell should not be active on nuc

--- nixos-shared/zsh.nix
+++ nixos-shared/zsh.nix
@@ -1,56 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  modifiedZbell = pkgs.writeText "modified-zbell.sh" ''
-#!/usr/bin/env zsh
-[[ -o interactive ]] || return
-
-# get $EPOCHSECONDS. builtins are faster than date(1)
-zmodload zsh/datetime || return
-
-autoload -Uz add-zsh-hook || return
-
-(( ''${+zbell_duration} )) || zbell_duration=15
-
-(( ''${+zbell_ignore} )) || zbell_ignore=($EDITOR $PAGER vim vi emacs less)
-
-zbell_timestamp=$EPOCHSECONDS
-
-# right before we begin to execute something, store the time it started at
-zbell_begin() {
-  zbell_timestamp=$EPOCHSECONDS
-  zbell_lastcmd=$1
-}
-
-# when it finishes, if it's been running longer than $zbell_duration,
-# and we dont have an ignored command in the line, then print a bell.
-zbell_end() {
-        LAST_EC=$?
-  ran_long=$(( $EPOCHSECONDS - $zbell_timestamp >= $zbell_duration ))
-
-  has_ignored_cmd=0
-  for cmd in ''${(s:;:)zbell_lastcmd//|/;}; do
-    words=(''${(z)cmd})
-    util=''${words[1]}
-    if (( ''${zbell_ignore[(i)$util]} <= ''${#zbell_ignore} )); then
-      has_ignored_cmd=1
-      break
-    fi
-  done
-
-  if (( ! $has_ignored_cmd )) && (( ran_long )); then
-                if [[ "$LAST_EC" == 0 ]]; then
-                  ${pkgs.libnotify}/bin/notify-send -u low "Command finished [$LAST_EC]" "$zbell_lastcmd"
-                else
-                  ${pkgs.libnotify}/bin/notify-send -u critical "Command failed [$LAST_EC]" "$zbell_lastcmd"
-                fi
-    print -n "\a"
-  fi
-}
-
-add-zsh-hook preexec zbell_begin
-add-zsh-hook precmd zbell_end
-'';
   customEraseWord = pkgs.writeText "custom-erase-word.zsh" ''
     # TODO: unfortunately makes M-W etc not work for autosuggestions
     my-backward-delete-word() {
