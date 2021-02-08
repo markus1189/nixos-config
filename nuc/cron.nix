d1ca445c43f9dce4a9a79003050862b95bde5789
Markus Hauck <markus1189@gmail.com>
2021-02-08 19:37:21 +0100

    nuc: Add cron for kodi

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -3,6 +3,7 @@
 let
   downloadDir = "$HOME/Downloads";
   killall = "${pkgs.psmisc}/bin/killall";
+  kodi = "${pkgs.kodi}/bin/kodi";
   pgrep = "${pkgs.procps}/bin/pgrep";
   find = "${pkgs.findutils}/bin/find";
   xargs = "${pkgs.findutils}/bin/xargs";
@@ -17,6 +18,10 @@ in
     cron = {
       enable = true;
       systemCronJobs = [
+        " 0           3 * *   * ${userName} ${killall} kodi kodi.bin kodi-x11 2>/dev/null"
+        " 5           3 * *   * ${userName} ${killall} -9 kodi kodi.bin kodi-x11 2>/dev/null"
+        " 5          20 * *   * ${userName} ${pgrep} kodi || DISPLAY=:0 ${kodi} &>/dev/null &"
+        " 0           * * *   * ${userName} ${find} ${downloadDir} -type f \\( -iname '*.idx' -or -iname '*.sfv' -or -iname '*.url' -or -iname '*.rev' -or -iname '*[.-]sample.[ma][kv][vi]' \\) -delete"
         " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
         "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
         "0 8 * * * ${userName} ${find} /media/backups/Photos/web -name \"*$(date -d '-1 year' +%Y%m%d)*\" | ${xargs} ${telegramSendPhoto}"
