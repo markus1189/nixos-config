cf0487029bb79513fb6dcbbb857fed2756a1bf68
Markus Hauck <markus1189@gmail.com>
2021-02-14 09:25:46 +0100

    nuc: remove kodi cron

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -3,7 +3,6 @@
 let
   downloadDir = "$HOME/Downloads";
   killall = "${pkgs.psmisc}/bin/killall";
-  kodi = "${pkgs.kodi}/bin/kodi";
   pgrep = "${pkgs.procps}/bin/pgrep";
   find = "${pkgs.findutils}/bin/find";
   xargs = "${pkgs.findutils}/bin/xargs";
@@ -18,9 +17,6 @@ in
     cron = {
       enable = true;
       systemCronJobs = [
-        " 0           3 * *   * ${userName} ${killall} kodi kodi.bin kodi-x11 2>/dev/null"
-        " 5           3 * *   * ${userName} ${killall} -9 kodi kodi.bin kodi-x11 2>/dev/null"
-        " 5          20 * *   * ${userName} ${pgrep} kodi || env DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 ${kodi} &>/dev/null &"
         " 0           * * *   * ${userName} ${find} ${downloadDir} -type f \\( -iname '*.idx' -or -iname '*.sfv' -or -iname '*.url' -or -iname '*.rev' -or -iname '*[.-]sample.[ma][kv][vi]' \\) -delete"
         " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
         "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
