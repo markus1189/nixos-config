b3ae16f7cc03fdcbeeb62e482a0ba886584af08f
Markus Hauck <markus1189@gmail.com>
2021-02-24 11:37:04 +0100

    nuc: latest kernel

--- nuc/configuration.nix
+++ nuc/configuration.nix
@@ -42,6 +42,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot = { kernelPackages = pkgs.linuxPackages_latest; };
 
   networking = {
     hostName = "nuc";
