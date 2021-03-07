c1abefad5a69b80803e8c66cecea38b05e1d8d59
Markus Hauck <markus1189@gmail.com>
2021-03-07 09:26:43 +0100

    nuc: activate script

--- nuc/activate.sh
+++ nuc/activate.sh
@@ -1,1 +1,3 @@
-sudo nixos-rebuild -I nixos-config=/home/mediacenter/repos/nixos-config/nuc/configuration.nix --upgrade switch
+echo activating "$(date)"
+sudo nixos-rebuild -I nixos-config=/home/mediacenter/repos/nixos-config/nuc/configuration.nix switch
+echo activated "$(date)"
