ad151856edf3306ef5ed0b3e533a269b12a7b20d
Markus Hauck <markus1189@gmail.com>
2021-04-14 09:56:47 +0200

    Use regular bluetooth again

--- laptop/bluetooth.nix
+++ laptop/bluetooth.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ...}:
 
 let
-  # Fix bluez disconnects from staging, until it's reached the system channel...
-  bluezPkg = import (builtins.fetchurl "https://raw.githubusercontent.com/NixOS/nixpkgs/3e0b9f5/pkgs/os-specific/linux/bluez/default.nix");
-  bluez58 = pkgs.callPackage bluezPkg {};
 in
 {
   hardware = {
     bluetooth = {
       enable = true;
-      package = bluez58;
+      package = pkgs.bluezFull;
       settings = {
         General = {
           ControllerMode = "bredr";
