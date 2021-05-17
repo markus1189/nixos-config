3969ce38de76c698905a43240f66039149a203e0
Markus Hauck <markus1189@gmail.com>
2021-05-17 18:26:00 +0200

    kmonad: Start two units for internal/external

--- nixos-shared/packages/kmonad/service.nix
+++ nixos-shared/packages/kmonad/service.nix
@@ -2,14 +2,23 @@
 
 let
   kmonad = pkgs.haskell.lib.unmarkBroken (pkgs.haskell.lib.doJailbreak pkgs.haskellPackages.kmonad);
-  myConfig = pkgs.mutate ./markus.kbd { inputDeviceFile = "/dev/input/event0"; };
+  myConfigExternal = pkgs.mutate ./markus.kbd { inputDeviceFile = "/dev/input/by-id/usb-Lenovo_ThinkPad_Compact_USB_Keyboard_with_TrackPoint-event-kbd"; };
+  myConfigInternal = pkgs.mutate ./markus.kbd { inputDeviceFile = "/dev/input/event0"; };
 in
 
 {
-  systemd.services.mykmonad = {
-    description = "my custom kmonad unit";
+  systemd.services.mykmonadexternal = {
+    description = "my custom kmonad unit for external";
     serviceConfig = {
-      ExecStart = "${kmonad}/bin/kmonad ${myConfig}";
+      ExecStart = "${kmonad}/bin/kmonad ${myConfigExternal}";
+      Restart = "always";
+    };
+  };
+
+  systemd.services.mykmonadinternal = {
+    description = "my custom kmonad unit for internal";
+    serviceConfig = {
+      ExecStart = "${kmonad}/bin/kmonad ${myConfigInternal}";
       Restart = "always";
     };
   };
