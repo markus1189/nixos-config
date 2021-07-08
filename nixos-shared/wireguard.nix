8d95acd499dfaeb61b5416532e1d13c7e971546a
Markus Hauck <markus1189@gmail.com>
2021-07-08 18:27:18 +0200

    wireguard: Only use one for now

--- nixos-shared/wireguard.nix
+++ nixos-shared/wireguard.nix
@@ -7,15 +7,15 @@ in
   networking = {
     wg-quick = {
       interfaces = {
-        wg0 = {
-          address = [ "10.67.44.173/32" "fc00:bbbb:bbbb:bb01::4:2cac/128" ];
+        wg-us101 = {
+          address = [ "10.68.64.184/32" "fc00:bbbb:bbbb:bb01::5:40b7/128" ];
           dns = [ "10.64.0.1" ];
-          privateKey = secrets.wireguard.mozilla.privateKey;
+          privateKey = secrets.wireguard.mozilla.privateKeys.us101;
 
           peers = [{
             allowedIPs = [ "0.0.0.0/0" "::0/0" ];
-            endpoint = "194.110.113.51:51820";
-            publicKey = "3DsVxyx5NU+RiwTKqUKqG8/9yiEk+SFd+21DEZ0dAwo=";
+            endpoint = "86.106.121.158:51820";
+            publicKey = "iuFDwOiNNnWfTmtprPCLscUjonu+KKfXu39TL/SSzh4=";
           }];
         };
       };
