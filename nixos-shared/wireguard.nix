c07941023e6fc83eed8856b708f8cbbe6b3e08e4
Markus Hauck <markus1189@gmail.com>
2021-07-09 09:50:07 +0200

    Update wireguard

--- nixos-shared/wireguard.nix
+++ nixos-shared/wireguard.nix
@@ -8,17 +8,7 @@ in
   networking = {
     wg-quick = {
       interfaces = {
-        wg-us101 = {
-          address = [ "10.68.64.184/32" "fc00:bbbb:bbbb:bb01::5:40b7/128" ];
-          dns = [ "10.64.0.1" ];
-          privateKey = secrets.wireguard.mozilla.${host}.privateKeys.us101;
-
-          peers = [{
-            allowedIPs = [ "0.0.0.0/0" "::0/0" ];
-            endpoint = "86.106.121.158:51820";
-            publicKey = "iuFDwOiNNnWfTmtprPCLscUjonu+KKfXu39TL/SSzh4=";
-          }];
-        };
+        wg-us101 = secrets.wireguard.mozilla.${host}.wg-us101;
       };
     };
   };
