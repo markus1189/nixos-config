34b0ae2d8a10138a198cfeccbbd6b87f59e439b9
Markus Hauck <markus1189@gmail.com>
2021-11-01 14:34:04 +0100

    hosts: allow split.io for miro feature flags

--- laptop/hosts.nix
+++ laptop/hosts.nix
@@ -4,7 +4,7 @@ let
   hostsFile = "${pkgs.ndtSources.hosts}/alternates/fakenews-gambling-porn/hosts";
   modifiedHosts = pkgs.runCommand "filtered-hosts" {} ''
     # Accept this because of farnam street links...
-    ${pkgs.gnused}/bin/sed -e '/link.m.convertkit/d' -e '/links.strava.com/d' ${hostsFile} > $out
+    ${pkgs.gnused}/bin/sed -e '/link.m.convertkit/d' -e '/links.strava.com/d' -e '/\.split\.io/d' ${hostsFile} > $out
   '';
 in
 {
