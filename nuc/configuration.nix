dee9e139575b0ec7e7d537b2caca75e15b04bae9
Markus Hauck <markus1189@gmail.com>
2021-11-28 09:10:29 +0100

    nuc: Enable sysrq

--- nuc/configuration.nix
+++ nuc/configuration.nix
@@ -45,6 +45,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl."kernel.sysrq" = 1;
 
   networking = {
     hostName = "nuc";
