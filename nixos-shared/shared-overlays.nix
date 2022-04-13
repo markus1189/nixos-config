ce3ae4557a4c1af4ccdc9564f495d3722925c89f
Markus Hauck <markus1189@gmail.com>
2022-04-13 21:04:09 +0200

    (ndt) pin nixpkgs version

--- nixos-shared/shared-overlays.nix
+++ nixos-shared/shared-overlays.nix
@@ -1,11 +1,18 @@
 rec {
-  ndtOverlay = self: super: {
-    ndt = import (builtins.fetchTarball
-      "https://github.com/markus1189/ndt/archive/master.tar.gz") {
-        nixpkgs = self;
-        ghc = "ghc884";
-      };
-  };
+  ndtOverlay = self: super:
+    let
+      pinnedPkgs = import (builtins.fetchGit {
+        name = "nixpkgs-pinned-for-ndt";
+        url = "https://github.com/nixos/nixpkgs/";
+        rev = "4d60081494259c0785f7e228518fee74e0792c1b";
+      }) {};
+    in {
+      ndt = import (builtins.fetchTarball
+        "https://github.com/markus1189/ndt/archive/master.tar.gz") {
+          nixpkgs = pinnedPkgs;
+          ghc = "ghc884";
+        };
+    };
 
   ndtSourcesOverlay = self: super: {
     ndtSources = import ../ndt/sources.nix { };
@@ -32,7 +39,8 @@ rec {
   visidataOverlay = self: super:
     let pkgs = with self.python3Packages; [ requests sh pytimeparse ];
     in {
-      visidata = builtins.trace "INFO: Using visidata overlay for more python packages"
+      visidata =
+        builtins.trace "INFO: Using visidata overlay for more python packages"
         super.visidata.overridePythonAttrs
         (old: { propagatedBuildInputs = old.propagatedBuildInputs ++ pkgs; });
     };
