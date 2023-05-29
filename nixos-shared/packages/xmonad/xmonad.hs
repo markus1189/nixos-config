26cf702e036852f1f0b02dd5e39c9274cc2803ba
Markus Hauck <markus1189@gmail.com>
2023-05-29 18:52:57 +0200

    xmonad: clipmenu history size = 30

--- nixos-shared/packages/xmonad/xmonad.hs
+++ nixos-shared/packages/xmonad/xmonad.hs
@@ -325,7 +325,7 @@ myKeys =
     ((myModShift, xK_u), scratchTermUpper),
     ((myModShift, xK_w), shiftNextScreen),
     ((myModShift, xK_x), spawn "@xkill@/bin/xkill"),
-    ((myModShiftCtrl, xK_h), spawn "env CM_LAUNCHER=rofi CM_HISTLENGTH=20 @clipmenu@/bin/clipmenu"),
+    ((myModShiftCtrl, xK_h), spawn "env CM_LAUNCHER=rofi CM_HISTLENGTH=30 @clipmenu@/bin/clipmenu"),
     ((myModShiftCtrl, xK_q), spawn "@xmonadReset@/bin/xmonadReset"),
     -- Copy to all, kill again
     ((myModKey, xK_a), windows copyToAll),
