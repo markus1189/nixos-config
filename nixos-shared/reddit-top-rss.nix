55369275cc3c23eb2cd8dc28c356e66692a58cb5
Markus Hauck <markus1189@gmail.com>
2023-06-04 20:00:05 +0200

    (redditTopRss) add env

--- nixos-shared/reddit-top-rss.nix
+++ nixos-shared/reddit-top-rss.nix
@@ -1,11 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  secrets = import ./secrets.nix;
+in
 {
   virtualisation.oci-containers.containers = {
     redditTopRss = {
       autoStart = true;
       image = "johnny5w/reddit-top-rss:latest";
       ports = [ "9999:8080" ];
+      environment = {
+        REDDIT_USER = secrets.reddit.redditTopRss.user;
+        REDDIT_CLIENT_ID = secrets.reddit.redditTopRss.clientId;
+        REDDIT_CLIENT_SECRET = secrets.reddit.redditTopRss.clientSecret;
+      };
     };
   };
 }
