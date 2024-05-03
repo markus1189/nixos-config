d7ddd20eab6d17221c87cb48364dde4ceafd5f69
Markus Hauck <markus1189@gmail.com>
2024-05-03 11:02:04 +0200

    kodi: Force alsa

--- nuc/cron.nix
+++ nuc/cron.nix
@@ -21,7 +21,7 @@ in
       systemCronJobs = [
         " 0           3 * *   * ${userName} ${killall} kodi kodi.bin kodi-x11 2>/dev/null"
         " 5           3 * *   * ${userName} ${killall} -9 kodi kodi.bin kodi-x11 2>/dev/null"
-        " 0          20 * *   * ${userName} ${pgrep} kodi || env DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 ${kodi} &>/dev/null &"
+        " 0          20 * *   * ${userName} ${pgrep} kodi || env KODI_AE_SINK=ALSA DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 ${kodi} &>/dev/null &"
         " 0           * * *   * ${userName} ${find} ${downloadDir} -type f \\( -iname '*.idx' -or -iname '*.sfv' -or -iname '*.url' -or -iname '*.rev' -or -iname '*[.-]sample.[ma][kv][vi]' \\) -delete"
         " 5  4,10,16,22 * *   * ${userName} ${find} ${downloadDir} -mindepth 2 -type f \\( -iname \"*.mkv\" -or -iname \"*.avi\" \\) -exec mv -v {} ${downloadDir} ';'"
         "35           4 * *   * ${userName} ${find} ${downloadDir} -type d -empty -delete"
