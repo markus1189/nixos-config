5a0affdac0bacc2416a86fbe1d1a6903dcc05725
Markus Hauck <markus1189@gmail.com>
2024-06-19 18:49:41 +0200

    ripggrepFzf

--- nixos-shared/common-packages.nix
+++ nixos-shared/common-packages.nix
@@ -1,52 +1,52 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   environment = {
-    systemPackages = with pkgs; [
-      aws-vault
-      bat
-      binutils
-      cachix
-      dnsutils
-      ed
-      edbrowse
-      gnupg
-      gnupg1
-      gitFull
-      (gitAndTools.gh)
-      (gitAndTools.git-extras)
-      (gitAndTools.git-open)
-      (gitAndTools.hub)
-      git-secret
-      gron
-      htop
-      httpie
-      iotop
-      jo
-      jq
-      jless
-      moreutils
-      ncdu
-      nix-prefetch-docker
-      nix-prefetch-git
-      nix-prefetch-github
-      nix-index
-      nixfmt
-      nixpkgs-lint
-      nixpkgs-fmt
-      niv
-      notifySendPb
-      notifySendTelegram
-      sendTelegramPoll
-      viessmannOutsideTemperature
-      telegramPhotosLastYear
-      pwgen
-      yq
-      (myScripts.logArgs)
-      viddy
-      visidata
-      watchexec
-      wormhole-william
-    ];
+    systemPackages = with pkgs;
+      [
+        aws-vault
+        bat
+        binutils
+        cachix
+        dnsutils
+        ed
+        edbrowse
+        gnupg
+        gnupg1
+        gitFull
+        (gitAndTools.gh)
+        (gitAndTools.git-extras)
+        (gitAndTools.git-open)
+        (gitAndTools.hub)
+        git-secret
+        gron
+        htop
+        httpie
+        iotop
+        jo
+        jq
+        jless
+        moreutils
+        ncdu
+        nix-prefetch-docker
+        nix-prefetch-git
+        nix-prefetch-github
+        nix-index
+        nixfmt
+        nixpkgs-lint
+        nixpkgs-fmt
+        niv
+        notifySendPb
+        notifySendTelegram
+        sendTelegramPoll
+        viessmannOutsideTemperature
+        telegramPhotosLastYear
+        pwgen
+        yq
+        viddy
+        visidata
+        watchexec
+        wormhole-william
+      ] ++ (with pkgs; [ (myScripts.logArgs) (myScripts.ripgrepFzf) ]);
   };
 }
