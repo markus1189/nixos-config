f768d0aa1ff8176c0b2a60f8b5519b31075eb3d1
Markus Hauck <markus1189@gmail.com>
2024-07-18 16:52:10 +0200

    tmux: extrakto config

--- nixos-shared/packages/tmux/service.nix
+++ nixos-shared/packages/tmux/service.nix
@@ -25,11 +25,13 @@
       extraConfig = with pkgs; ''
         ${pkgs.lib.readFile ./tmux.conf}
         run-shell ${tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
-        run-shell ${tmuxPlugins.copycat}/share/tmux-plugins/copycat/copycat.tmux
 
         run-shell ${tmuxPlugins.extrakto}/share/tmux-plugins/extrakto/extrakto.tmux
-        set -g @extrakto_copy_key "tab"
-        set -g @extrakto_insert_key "enter"
+        set -g @extrakto_copy_key "enter"
+        set -g @extrakto_insert_key "tab"
+
+        set -g @extrakto_grab_area "recent"
+        set -g @extrakto_filter_order "path url line word quote s-quote all"
       '';
     };
   };
