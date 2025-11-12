239a8dcb762eacd6564024758d76ebc28485897b
Markus Hauck <markus1189@gmail.com>
2025-11-12 08:47:33 +0100

    packages: use opencode from master

--- nixos-shared/common-packages.nix
+++ nixos-shared/common-packages.nix
@@ -17,7 +17,7 @@ in
 {
   environment = {
     systemPackages =
-      (with nixpkgsMaster; [ claude-code ])
+      (with nixpkgsMaster; [ claude-code opencode ])
       ++ (with pkgs; [
         aws-vault
         bat
