776df28e74793441e7566bdd7ae21644ad45970d
Markus Hauck <markus1189@gmail.com>
2025-12-03 19:26:58 +0100

    wireguard: really update

--- nixos-shared/wireguard.nix
+++ nixos-shared/wireguard.nix
@@ -1,6 +1,13 @@
 host:
 { config, pkgs, ... }:
 
+# Generate new relay server lists:
+# 1) Cleanup existing files: 
+# rm -v ~/mozwire/*.conf
+# 2) Save the new files:
+# nix shell nixpkgs#mozwire nixpkgs#wireguard-tools nixpkgs#remarshal --command "mozwire" relay save -o ~/mozwire 
+# 3) Update ./secrets.nix with the content of the file
+
 let
   secrets = import ./secrets.nix;
 in
