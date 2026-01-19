5ee6ed49790f23ce3af40943ce984a3cca041651
Markus Hauck <markus1189@gmail.com>
2026-01-19 09:03:32 +0100

    opencode: use dunstify

--- nixos-shared/home-manager/opencode/terminal-bell.ts
+++ nixos-shared/home-manager/opencode/terminal-bell.ts
@@ -5,7 +5,7 @@ export const TerminalBell: Plugin = async ({ project, client, $, directory, work
     event: async ({ event }) => {
       if (event.type === "session.idle") {
         await Bun.write(Bun.stdout, "\x07")
-        await $`notify-send Opencode "Session Idle"`
+        await $`dunstify Opencode "Session Idle"`
       }
     }
   }
