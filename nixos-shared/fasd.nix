b77ecc5b96f5bdf13cbbe2a1fe028375aa2d667e
Markus Hauck <markus1189@gmail.com>
2026-09-26 10:01:00 +0200

    fasd: record $PWD, not the repo root, under the whjvenyl fork

--- nixos-shared/fasd.nix
+++ nixos-shared/fasd.nix
@@ -4,6 +4,8 @@
   environment = {
     systemPackages = with pkgs; [ fasd ];
     interactiveShellInit = ''
+      # The whjvenyl fork records a repo's root instead of $PWD by default
+      export _FASD_ONLY_VCS=0
       eval "$(${pkgs.fasd}/bin/fasd --init auto)"
     '';
   };
